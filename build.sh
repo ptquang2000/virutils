3b2c9d5633844ac502572ef018478b3cbeb25f7e
quang.phan <ptquang2000@gmail.com>
2026-08-12 21:27:20 +0700

    upd send command

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 pushd build >/dev/null
-clang++ -fproc-stat-report -g -O0 -glldb -std=c++23 ../main.cpp -lvirt-qemu -lvirt -o virutil
+clang++ -fproc-stat-report -g -O0 -glldb -std=c++23 ../main.cpp -lglib-2.0 -lvirt-qemu -lvirt -o virutil
 popd >/dev/null
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #include <libvirt/libvirt.h>
+#include <libvirt/libvirt-qemu.h>
 
 #include "third_party/json.h"
 #include "third_party/tinyxml2.cpp"
@@ -20,7 +21,7 @@
 #define u32	uint32_t
 #define u64	uint64_t
 
-static constexpr std::string_view k_domainName =  "win11-tiny";
+static constexpr std::string_view k_domainName =  "win11-24h2";
 static constexpr std::string_view k_socketFormat = 
 "/run/libvirt/qemu/channel/{}-{}/org.qemu.guest_agent.0";
 
@@ -31,84 +32,24 @@ main(int argc, char **argv)
 	); auto pDomain = virDomainLookupByName(
 		pConnection, k_domainName.data()))
 	{
-		auto socketName = std::format(
-			k_socketFormat.data(),
-			virDomainGetID(pDomain),
-			virDomainGetName(pDomain));
-		sockaddr_un addr{};
-		addr.sun_family = AF_UNIX;
-		std::strncpy(
-			addr.sun_path,
-			socketName.c_str(),
-			sizeof(addr.sun_path) - 1);
-		
-		if (int dataSocket = socket(AF_UNIX, SOCK_STREAM, 0
-		); dataSocket != -1)
+
+		nlohmann::json guestExecCommand
 		{
-			if (int ret = connect(
-				dataSocket, 
-				reinterpret_cast<sockaddr *>(&addr),
-				sizeof(addr)
-			); ret == -1)
-			{
-				perror("Failed to connect socket");
-				return errno;
-			}
-
-			nlohmann::json guestExecCommand
-			{
-				{"execute", "guest-exec"},
-				{"arguments", {
-						{"path", "/bin/sh"},
-						{"arg", {"-c", "id"}},
-						{"capture-output", true}
-				}}
-			};
-			auto request = guestExecCommand.dump() + '\n';
-			auto command = std::string_view(request);
-			while (!command.empty())
-			{
-				if (size_t written = write(dataSocket, 
-							command.data(), 
-							command.size()
-				); written > 0)
-				{
-					command.remove_prefix(written);
-				}
-				else
-				{
-					perror("Failed send command");
-					return errno;
-				}
-			}
-
-			std::string buffer(4096, '\0');
-			for (;;)
-			{
-				if (auto end = buffer.find('\n'
-				); end != std::string::npos)
-				{
-					auto line = buffer.substr(0, end);
-					buffer.erase(0, end + 1);
-					break;
-				}
-				std::array<char, 4096> chunk;
-				ssize_t got = read(dataSocket,
-						chunk.data(),
-						chunk.size());
-				if (got == 0)
-				{
-					break;
-				}
-				if (got < 0)
-				{
-					perror("Failed to read response");
-					return {};
-				}
-				buffer.append(chunk.data(), got);
-			}
-			close(dataSocket);
-		}
+			{"execute", "guest-exec"},
+			{"arguments", {
+					{"path", "/bin/sh"},
+					{"arg", {"-c", "id"}},
+					{"capture-output", true}
+			}}
+		};
+		std::string command = guestExecCommand.dump();
+		char *reply = virDomainQemuAgentCommand(
+				pDomain,
+				command.c_str(),
+				VIR_DOMAIN_QEMU_AGENT_COMMAND_DEFAULT,
+				VIR_DOMAIN_QEMU_MONITOR_COMMAND_DEFAULT);
+		auto err = virGetLastError();
+		free(reply);
 	}
 	else
 	{
