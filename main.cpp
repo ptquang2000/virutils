42f53b3da9abb954cd4f4bdf31b5ed1d690f43c3
quang.phan <ptquang2000@gmail.com>
2026-08-12 13:10:57 +0700

    add write/read socket

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <print>
-#include <filesystem>
 
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -21,37 +20,101 @@
 #define u32	uint32_t
 #define u64	uint64_t
 
+static constexpr std::string_view k_domainName =  "win11-tiny";
+static constexpr std::string_view k_socketFormat = 
+"/run/libvirt/qemu/channel/{}-{}/org.qemu.guest_agent.0";
+
 int 
 main(int argc, char **argv)
 {
 	if (auto pConnection = virConnectOpen("qemu:///system"
-	); auto pDomain = virDomainLookupByName(pConnection, "win11-24h2"))
+	); auto pDomain = virDomainLookupByName(
+		pConnection, k_domainName.data()))
 	{
 		auto socketName = std::format(
-			"/run/libvirt/qemu/channel/{}-{}/org.qemu.guest_agent.0",
+			k_socketFormat.data(),
 			virDomainGetID(pDomain),
-			virDomainGetName(pDomain)
-		);
+			virDomainGetName(pDomain));
 		sockaddr_un addr{};
 		addr.sun_family = AF_UNIX;
 		std::strncpy(
 			addr.sun_path,
 			socketName.c_str(),
-			sizeof(addr.sun_path) - 1
-		);
+			sizeof(addr.sun_path) - 1);
 		
 		if (int dataSocket = socket(AF_UNIX, SOCK_STREAM, 0
 		); dataSocket != -1)
 		{
 			if (int ret = connect(
-				dataSocket, (sockaddr *)&addr, sizeof(addr)
+				dataSocket, 
+				reinterpret_cast<sockaddr *>(&addr),
+				sizeof(addr)
 			); ret == -1)
 			{
-				perror("connect");
+				perror("Failed to connect socket");
 				return errno;
+			}
+
+			nlohmann::json guestExecCommand
+			{
+				{"execute", "guest-exec"},
+				{"arguments", {
+						{"path", "/bin/sh"},
+						{"arg", {"-c", "id"}},
+						{"capture-output", true}
+				}}
+			};
+			auto request = guestExecCommand.dump() + '\n';
+			auto command = std::string_view(request);
+			while (!command.empty())
+			{
+				if (size_t written = write(dataSocket, 
+							command.data(), 
+							command.size()
+				); written > 0)
+				{
+					command.remove_prefix(written);
+				}
+				else
+				{
+					perror("Failed send command");
+					return errno;
+				}
+			}
+
+			std::string buffer(4096, '\0');
+			for (;;)
+			{
+				if (auto end = buffer.find('\n'
+				); end != std::string::npos)
+				{
+					auto line = buffer.substr(0, end);
+					buffer.erase(0, end + 1);
+					break;
+				}
+				std::array<char, 4096> chunk;
+				ssize_t got = read(dataSocket,
+						chunk.data(),
+						chunk.size());
+				if (got == 0)
+				{
+					break;
+				}
+				if (got < 0)
+				{
+					perror("Failed to read response");
+					return {};
+				}
+				buffer.append(chunk.data(), got);
 			}
 			close(dataSocket);
 		}
+	}
+	else
+	{
+		std::println(stderr,
+				"Failed to connect domain {}",
+				k_domainName);
 	}
 	return 0;
 }
